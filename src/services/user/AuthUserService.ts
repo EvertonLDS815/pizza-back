30b29d4f10d568c6e36189ce04594bad75f18fc4
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-04 20:45:12 -0300

    feat: jwt authenticated with hash and dotenv

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../../prisma';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface AuthRequest {
   email: string;
@@ -27,7 +28,25 @@ class AuthUserService {
       return { error: 'User/Password incorrect!' };
     }
 
-    return { ok: true };
+    // Se tudo deu certo será gerado o token de usuário
+
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.SECRET_JWT,
+      {
+        subject: user.id,
+        expiresIn: '30d',
+      }
+    );
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
