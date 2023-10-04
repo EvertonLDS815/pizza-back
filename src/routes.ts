1a2c3bd39be08411b3c1e00edd7a5b0428332105
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-04 20:59:36 -0300

    feat: add routes "get" to return data

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUsercontroller';
+import { DetailUserController } from './controllers/user/DetailUserController';
 
 const router = Router();
 
 // rotas user
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
+router.get('/me', new DetailUserController().handle);
 
 export { router };
