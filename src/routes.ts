80ed7ca26addf840183d239f66af3d49a1e8ddfb
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-04 21:36:40 -0300

    feat: add middleware and check token - part 1

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUsercontroller';
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 // rotas user
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
-router.get('/me', new DetailUserController().handle);
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 export { router };
