0b62d0389d0781fe9d0acafd524adfad0aa22d08
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-08 22:43:26 -0300

    feat: router - Finish Order

--- src/routes.ts
+++ src/routes.ts
@@ -15,6 +15,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrderController } from './controllers/order/ListOrdersController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 import uploadConfig from './config/multer';
 
@@ -63,6 +64,11 @@ router.get(
   '/order/detail',
   isAuthenticated,
   new DetailOrderController().handle
+);
+router.put(
+  '/order/finish',
+  isAuthenticated,
+  new FinishOrderController().handle
 );
 
 export { router };
