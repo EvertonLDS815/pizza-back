85f2aedbb088df1d85bc99a1a0a9615b5b7fedc7
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-08 21:37:59 -0300

    feat: router - Send Order

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,7 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 import uploadConfig from './config/multer';
 
@@ -54,4 +55,5 @@ router.delete(
   isAuthenticated,
   new RemoveItemController().handle
 );
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 export { router };
